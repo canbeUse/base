9370e163605c2269eb019e7ce392816a0821b7e7
pan <details@gmail.com>
2015-08-07 15:12:37 +0800

    create base-domain

---
+++ base-domain/src/main/java/base/domain/basenum/DayEnum.java
@@ -0,0 +1,28 @@
+package base.domain.basenum;
+
+/**
+ * Created by working on 2015-07-23.
+ */
+public enum DayEnum {
+    Monday(1,"星期一"),
+    Tuesday(2,"星期二"),
+    Wendesday(3,"星期三"),
+    Thrusday(4,"星期四"),
+    Friday(5,"星期五"),
+    Saturday(6,"星期六"),
+    Sunday(7,"星期日");
+    private int value;
+    private String name;
+
+    public int getValue() {
+        return value;
+    }
+    public String getName() {
+        return name;
+    }
+
+    DayEnum(int value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+}
