34856890be42f95f0861328e5c19dab2ee7e23f5
pan <details@gmail.com>
2015-08-13 17:53:30 +0800

    add XmlUtil-xmlToObject

--- base-test/src/main/java/base/test/TestUtil.java
+++ base-test/src/main/java/base/test/TestUtil.java
@@ -22,7 +22,7 @@ public class TestUtil
     }
 
     @Test
-    public void testXmlUtil() {
+    public void testXmlUtil1() {
         User user = new User();//User需添加注释：@XmlRootElement
         user.setuId(1);
         user.setuName("111");
@@ -30,6 +30,20 @@ public class TestUtil
         try {
             System.out.println(XmlUitl.objectToXml(user, User.class));
         } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testXmlUtil2() {
+        String xmlStr = "<user>\n" +
+                "    <uId>1</uId>\n" +
+                "    <uName>111</uName>\n" +
+                "    <uPassword>111</uPassword>\n" +
+                "</user>";
+        try {
+            System.out.println(XmlUitl.xmlToObject(xmlStr, User.class));
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- base-util/src/main/java/base/util/XmlUitl.java
+++ base-util/src/main/java/base/util/XmlUitl.java
@@ -1,8 +1,12 @@
 package base.util;
 
+import org.xml.sax.InputSource;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 
@@ -14,10 +18,20 @@ public class XmlUitl {
         Writer writer = new StringWriter();
         JAXBContext context = JAXBContext.newInstance(clazz);
         Marshaller marshaller = context.createMarshaller();
-
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
         marshaller.marshal(t, writer);
-
         return writer.toString();
+    }
+
+    public static <T> T xmlToObject(String xml, Class<T> clazz)
+            throws IllegalAccessException, InstantiationException, JAXBException {
+        if (StringUtil.isNullOrEmpty(xml)) {
+            return clazz.newInstance();
+        }
+        StringReader xmlString = new StringReader(xml);
+        InputSource source = new InputSource(xmlString);
+        JAXBContext jc = JAXBContext.newInstance(clazz);
+        Unmarshaller u = jc.createUnmarshaller();
+        return (T) u.unmarshal(source);
     }
 }
