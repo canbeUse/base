a530ef43e594801bf577e4f4ffe5aeb4093d7a8d
pan <details@gmail.com>
2015-08-18 16:19:38 +0800

    add TestMain

---
+++ base-test/src/main/java/base/test/TestMain.java
@@ -0,0 +1,26 @@
+package base.test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by working on 2015-08-14.
+ */
+public class TestMain {
+
+    @Test
+    public void testStringLength(){
+        List<String> slist = new ArrayList<>();
+        slist.add("abc");
+        slist.add("123");
+        slist.add("一二三");
+        slist.add("abc一二三");
+
+        for(String s: slist){
+            System.out.println(s.length());
+        }
+    }
+}
+
